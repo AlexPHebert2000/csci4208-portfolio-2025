b705a5a9009c01736b5dcd70a0eef0a27260cd6a
Alex Hebert <alex.p.hebert.dev@gmail.com>
2025-09-29 20:14:56 -0500

    ADD background

--- labs/lab-5/src/PlayScene.js
+++ labs/lab-5/src/PlayScene.js
@@ -3,8 +3,16 @@ class PlayScene extends Phaser.Scene {
     super('play');
   }
 
-  preload(){}
-  create(){}
+  preload(){
+    this.load.path = 'assets/';
+    this.load.image('background', 'background.png')
+  }
+  create(){
+    this.create_map();
+  }
+  create_map() {
+    this.add.image(640/2, 480/2, 'background')
+  }
   update(){}
-  
+
 }
