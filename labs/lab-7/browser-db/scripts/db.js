9fcd35aacb294346b65ce62f414d8ca35685676c
Alex <alexphebert@gmail.com>
2025-10-15 16:46:32 -0500

    COMPLETE lab 7 goal 0

---
+++ labs/lab-7/browser-db/scripts/db.js
@@ -0,0 +1,31 @@
+export const boot = async () => {
+  throw new Error("boot() not implemented yet");
+}
+
+// Write
+export const insertOne = (col, data) => {
+  throw new Error("insertOne() not implemented yet");
+}
+
+// Read
+export const getDoc = () => {
+  throw new Error("getDoc() not implemented yet");
+}
+
+export const findMany = () => {
+  throw new Error("findMany() not implemented yet");
+}
+
+export const findOne = () => {
+  throw new Error("findOne() not implemented yet");
+}
+
+// Update
+export const updateOne = (col, id, patch) => {
+  throw new Error("updateOne() not implemented yet");
+}
+
+// Delete
+export const deleteOne = (col, id) => {
+  throw new Error("deleteOne() not implemented yet");
+}
